957120e9c7716c705047237320015f4894b606b6
EMONGAZI19 <afranemon84@gmail.com>
2025-04-20 15:00:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // script.js
 const video = document.getElementById('player');
-const hls = new Hls();
-const player = new Plyr(video);
+const hls = new Hls();  // HLS.js instance
+const player = new Plyr(video); // Plyr.js instance for advanced controls
 
+// Function to start stream from a given URL
 function playStream(url) {
-    hls.loadSource(url);
-    hls.attachMedia(video);
-    video.play();
+    if (Hls.isSupported()) {
+        hls.loadSource(url);  // Load the source from the stream URL
+        hls.attachMedia(video);  // Attach the media to the player
+        video.play();  // Play the video
+    }
+    // For browsers that don't support HLS.js (like Safari)
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = url;
+        video.play();
+    }
 }
 
 // Default stream on page load
