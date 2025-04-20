9241be38f90e6d133be391de78cb3fbc9b1a9173
EMONGAZI19 <afranemon84@gmail.com>
2025-04-20 14:57:34 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+const video = document.getElementById('player');
+const hls = new Hls();
+const player = new Plyr(video);
+
+function playStream(url) {
+    hls.loadSource(url);
+    hls.attachMedia(video);
+    video.play();
+}
+
+// Default stream on page load
+window.addEventListener('DOMContentLoaded', () => {
+    playStream('https://tv.bdixtv24.co/toffee/live.php?id=7e00fee81848&e=.m3u8');
+});
