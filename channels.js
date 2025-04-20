32e984b7b0e6d62aa23b8e796858a44f0bf04d64
EMONGAZI19 <afranemon84@gmail.com>
2025-04-21 00:47:50 +0600

    Update channels.js

--- channels.js
+++ channels.js
@@ -3,72 +3,72 @@ const channels = {
     {
       name: "Live Match 1",
       img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png",
-      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+      url: "http://kst.moonplex.net:8080/CH3/index.m3u8",
       isLive: true
     },
     {
       name: "Live Match 2",
       img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png",
-      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+      url: "http://kst.moonplex.net:8080/CH3/index.m3u8",
       isLive: true
     },
     {
       name: "Live Match 3",
       img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png",
-      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+      url: "http://kst.moonplex.net:8080/CH3/index.m3u8",
       isLive: true
     }
   ],
   "Bangladesh": [
-    { name: "BD Channel 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 6", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 7", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 8", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 9", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "BD Channel 10", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false }
+    { name: "BD Channel 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 6", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 7", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 8", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 9", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "BD Channel 10", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false }
   ],
   "Entertainment": [
-    { name: "Entertainment 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Entertainment 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Entertainment 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Entertainment 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Entertainment 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Entertainment 6", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Entertainment 7", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false }
+    { name: "Entertainment 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Entertainment 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Entertainment 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Entertainment 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Entertainment 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Entertainment 6", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Entertainment 7", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false }
   ],
   "Indian Bangla": [
-    { name: "Indian Bangla 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Indian Bangla 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Indian Bangla 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Indian Bangla 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Indian Bangla 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false }
+    { name: "Indian Bangla 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Indian Bangla 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Indian Bangla 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Indian Bangla 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Indian Bangla 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false }
   ],
   "Kids": [
-    { name: "Kids 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Kids 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Kids 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Kids 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false }
+    { name: "Kids 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Kids 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Kids 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Kids 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false }
   ],
   "Music": [
-    { name: "Music 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Music 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Music 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Music 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false }
+    { name: "Music 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Music 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Music 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Music 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false }
   ],
   "Sports": [
-    { name: "Sports 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 6", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 7", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 8", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 9", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false },
-    { name: "Sports 10", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "...", isLive: false }
+    { name: "Sports 1", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 2", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 3", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 4", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 5", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 6", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 7", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 8", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 9", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false },
+    { name: "Sports 10", img: "https://i.postimg.cc/d16Y2v56/20250421-001244.png", url: "http://kst.moonplex.net:8080/CH3/index.m3u8", isLive: false }
   ]
 };
