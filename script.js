ec10d239e35567153047be54d320168906006f37
EMONGAZI19 <afranemon84@gmail.com>
2025-04-21 11:23:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -130,6 +130,24 @@ function toggleMenu() {
   menu.classList.toggle("show");
 }
 
+document.querySelectorAll('.channel').forEach(channel => {
+  channel.addEventListener('click', function(e) {
+    const rect = this.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const ripple = document.createElement('span');
+    ripple.className = 'ripple';
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+    this.appendChild(ripple);
+
+    setTimeout(() => {
+      ripple.remove();
+    }, 600);
+  });
+});
+
 // Animate Live Slider
 function animateLiveSlider() {
   const slider = document.querySelector(".live-slider");
